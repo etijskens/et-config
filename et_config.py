0a9926fe4ff1669428088df14ec5f966bcc3f899
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2021-04-20 14:32:52 +0200

    wip

--- et_config.py
+++ et_config.py
@@ -7,14 +7,88 @@ A 'hello world' example.
 """
 __version__ = "0.0.0"
 
+from pathlib import Path
+import json
 
-def hello(who='world'):
-    """'Hello world' method.
+class Config:
+    """Class for configuration (or preferences) files.
 
-    :param str who: whom to say hello to
-    :returns: a string
-    """
-    result = "Hello " + who
-    return result
+    In fact little more than a persistent dict."""
+
+    # this key controls the file location of the Config file
+    file_key = 'file_loc'
+
+    def __init__(self, **kwargs ):
+        """"""
+        self.data = {}
+
+        if not kwargs:
+            self.load(p_cfg=None)
+        else:
+            if Config.file_key in kwargs:
+                # Read Config object data
+                p_cfg = Path(kwargs[Config.file_key])
+                self.load(p_cfg)
+
+            # Add the other kwargs
+            kwargs.pop(Config.file_key,None)
+            self.add(**kwargs)
+
+    def load(self, p_cfg=None):
+        """
+
+        :param Path p_cfg: path to cfg file
+        """
+        if p_cfg is None:
+            # first look into
+
+        self.p_cfg = p_cfg.resolve()
+        with p_cfg.open() as fp_cfg:
+            data = json.load(fp_cfg)
+            self.data.update(data)
+
+        self.update_location_(p_cfg)
+
+
+    def save(self, file='', mkdir=False):
+        """Save self.data to cfg file."""
+        p_cfg = self.p_cfg if not file else Path(file)
+        p_cfg = p_cfg.resolve()
+        if not p_cfg.parent.exists():
+            if mkdir:
+                p_cfg.parent.mkdir()
+            else:
+                print(f'Inexisting Path: {p_cfg.parent}. Create it yourself, or use the "mkdir=True" parameter.')
+                raise FileNotFoundError()
+        self.update_location_(p_cfg)
+
+        with p_cfg.open('w') as fp_cfg:
+            json.dump(self.data, fp_cfg, indent=2)
+
+
+    def add(self, **kwargs):
+        self.data.update(kwargs)
+
+
+    # "Private" methods: For internal use only.
+    def update_location_(self, p_cfg):
+        """Make sure self.data[Config.file_key] points to the correct location, i.e. where the Config object was
+        Read from or written to.
+
+        For internal use only.
+        """
+        self.data[Config.file_key] = str(p_cfg)
+
+
+    def __str__(self):
+        return json.dumps(self.data,indent=2)
+
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
 
 # eof
--- tests/test_et_config.py
+++ tests/test_et_config.py
@@ -2,21 +2,59 @@
 
 """Tests for et_config package."""
 
-import et_config
+import logging
+import os
+import shutil
+from pathlib import Path
+
+import pytest
+
+from et_config import Config
 
 
-def test_hello_noargs():
-    """Test for et_config.hello()."""
-    s = et_config.hello()
-    assert s=="Hello world"
+__test_workspace__ = (Path(__file__) / '../../test_workspace').resolve()
 
 
-def test_hello_me():
-    """Test for et_config.hello('me')."""
-    s = et_config.hello('me')
-    assert s=="Hello me"
-    
-    
+def clear_test_workspace(folder=None):
+    """If dir is None, clear the test workspace by removing it and recreating it.
+    Otherwise, only remove directory folder
+    """
+    if 'VSC_HOME' in os.environ:
+        # see https://stackoverflow.com/questions/58943374/shutil-rmtree-error-when-trying-to-remove-nfs-mounted-directory
+        logging.shutdown()
+
+    if not folder is None:
+        p = __test_workspace__ / folder
+        if p.exists():
+            shutil.rmtree(p)
+    else:
+        if __test_workspace__.exists():
+            shutil.rmtree(__test_workspace__)
+
+    __test_workspace__.mkdir(exist_ok=True)
+
+
+def test_Config_ctor():
+    """Test Config ctor."""
+    clear_test_workspace()
+
+    cfg = Config(first_name='bert',name='tijskens')
+    assert cfg.data['first_name'] == 'bert'
+    assert cfg.data['name'] == 'tijskens'
+
+    cfg.save(__test_workspace__ / '.cfg')
+
+    cfg_copy = Config(file_loc=__test_workspace__ / '.cfg')
+    cfg_copy.add(address='home')
+    print(cfg_copy)
+    with pytest.raises(FileNotFoundError):
+        print('Exception expected:')
+        cfg_copy.save(__test_workspace__ / 'address/.cfg')
+
+    cfg_copy.save(__test_workspace__ / 'address/.cfg', mkdir=True)
+
+
+
 # ==============================================================================
 # The code below is for debugging a particular test in eclipse/pydev.
 # (otherwise all tests are normally run with pytest)
@@ -24,7 +62,7 @@ def test_hello_me():
 # that the source directory is on the path
 # ==============================================================================
 if __name__ == "__main__":
-    the_test_you_want_to_debug = test_hello_noargs
+    the_test_you_want_to_debug = test_Config_ctor
 
     print("__main__ running", the_test_you_want_to_debug)
     the_test_you_want_to_debug()
